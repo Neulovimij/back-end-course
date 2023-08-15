966e78eff896b384c379ee9396717282ea5a53e6
[Alex Samal] <[alex.samal127@gmail.com]>
2023-08-15 22:10:57 +0300

    validation

--- src/routers/products-router.ts
+++ src/routers/products-router.ts
@@ -1,16 +1,24 @@
 import {Request, Response, Router} from "express";
 import {productsRepository} from "../repositories/products-repository";
+import {body, validationResult} from "express-validator";
+import {inputValidationMiddleware} from "../middlevares/input-validation-middleware";
 
 
 
 export const productsRouter = Router({})
+
+const titleValidation = body("title").isLength({min: 3, max: 30})
 
 productsRouter.get('/', (req: Request, res: Response) => {
     const foundProducts = productsRepository.findProducts(req.query.title?.toString())
         res.send(foundProducts)
 })
 
-productsRouter.post('/', (req: Request, res: Response) => {
+productsRouter.post('/',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+
     const newProduct = productsRepository.createProduct(req.body.title)
     res.status(201).send(newProduct)
 })
@@ -24,7 +32,11 @@ productsRouter.get('/:id', (req: Request, res: Response) => {
     }
 
 })
-productsRouter.put('/:id', (req: Request, res: Response) => {
+productsRouter.put('/:id',
+    titleValidation,
+    inputValidationMiddleware,
+    (req: Request, res: Response) => {
+
 const isUpdated = productsRepository.updateProduct(+req.params.id, req.body.title)
 if (isUpdated) {
     const product = productsRepository.findProductById(+req.params.id)
